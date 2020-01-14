d241b5d47f60445337f3a633da26e8f9faa2aa06
harry-pm <harrypenderedmazer@gmail.com>
2020-01-14 11:34:12 +0000

    add activity url

--- app.js
+++ app.js
@@ -9,17 +9,29 @@ var knex = require('knex')({
   })
 
 app.get('/', (req, res) => {
-    res.send("hello world")
+    res.send("To use this website, use /showall")
 });
 
 app.get('/showall', (req, res) => {
     knex('entries').select('*')
-    .then(function(shows) {
-        res.status(200).json(shows);
+    .then(function(activities) {
+        res.status(200).json(activities);
       })
       .catch(function(error) {
         next(error);
       });
+});
+
+app.post('/add', (req, res, next) => {
+    knex('entries')
+    .insert(req.query)
+    .then(function (){
+        res.redirect('/showAll')
+    })
+    .catch(function(error) {
+        next(error);
+      });
+    console.log(req.query)
 })
 
 app.listen(3000, () => console.log("listening on port 3000" ));
