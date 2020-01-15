a800688a2d1366937f491ae52c20156219938441
harry-pm <harrypenderedmazer@gmail.com>
2020-01-15 15:07:55 +0000

    form on front end, final mvp, ready for production level

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const app = express();
 const setupFile = require('./knexfile').development
 
 var knex = require('knex')({
-    client: setupFile.client,
-    connection: setupFile.connection,
-    pool: { min: 0, max: 7 }
-  })
+  client: setupFile.client,
+  connection: setupFile.connection,
+  pool: { min: 0, max: 7 }
+})
+
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
+app.use(express.urlencoded());
+
+const path = require('path');
+app.use(express.static(__dirname + '/views')) // html is a static file
 
 app.get('/', (req, res) => {
     res.send("To use this website, use /showall")
@@ -22,16 +29,21 @@ app.get('/showall', (req, res) => {
       });
 });
 
-app.post('/add', (req, res, next) => {
-    knex('entries')
-    .insert(req.query)
-    .then(function (){
-        res.redirect('/showAll')
-    })
-    .catch(function(error) {
-        next(error);
-      });
-    console.log(req.query)
+app.get('/add', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/add.html'))
+})
+
+app.post('/added', urlencodedParser, (req, res, next) => {
+  const entry = {activity: req.body.activity, description: req.body.description, location: req.body.location}  
+  knex('entries')
+  .insert(entry, 'id')
+  .then(function (){
+      res.redirect('/showAll')
+  })
+  .catch(function(error) {
+      next(error);
+    });
+  console.log(entry)
 })
 
 app.listen(3000, () => console.log("listening on port 3000" ));
