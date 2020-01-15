fd6c6c6e1b28520ecef86c59f02601bf7f4c253f
harry-pm <harrypenderedmazer@gmail.com>
2020-01-15 15:23:20 +0000

    launch page

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const app = express();
 const setupFile = require('./knexfile').development
-
 var knex = require('knex')({
   client: setupFile.client,
   connection: setupFile.connection,
   pool: { min: 0, max: 7 }
 })
-
 var bodyParser = require('body-parser')
 var urlencodedParser = bodyParser.urlencoded({ extended: true })
-app.use(express.urlencoded());
-
 const path = require('path');
-app.use(express.static(__dirname + '/views')) // html is a static file
+
+app.use(express.urlencoded());
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', (req, res) => {
-    res.send("To use this website, use /showall")
+    res.sendFile(path.join(__dirname + '/views/launch.html'))
 });
 
 app.get('/showall', (req, res) => {
@@ -43,7 +41,6 @@ app.post('/added', urlencodedParser, (req, res, next) => {
   .catch(function(error) {
       next(error);
     });
-  console.log(entry)
 })
 
 app.listen(3000, () => console.log("listening on port 3000" ));
